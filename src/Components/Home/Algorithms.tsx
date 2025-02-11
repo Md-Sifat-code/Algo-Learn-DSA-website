49aa01540b5f61d962c43dc3b3ad570acbd25ee0
Md-Sifat-code <codewithsifat4@gmail.com>
2025-02-11 17:20:04 +0600

    fixed some bugs

--- src/Components/Home/Algorithms.tsx
+++ src/Components/Home/Algorithms.tsx
@@ -48,40 +48,45 @@ const Algorithms: React.FC = () => {
 
   // Display the data with Code Mockup for pseudocode
   return (
-    <div className="p-4 mt-[100px] sm:px-4 md:px-6 lg:px-8 overflow-x-hidden">
-      {data.map((algorithm) => (
-        <div key={algorithm.id} className="mb-8 sm:mb-6">
-          <h1 className="text-3xl text-purple-700 font-bold mb-4 sm:text-2xl md:text-3xl flex flex-row items-center">
-            <GiCursedStar className="text-amber-500" />
-            {algorithm.title}
-          </h1>
-          <p className="mb-4 text-lg sm:text-base">
-            <strong>Algorithm:</strong>
-            <ul className="list-disc pl-6">
-              {Array.isArray(algorithm.algorithm) ? (
-                algorithm.algorithm.map((step: string, index: number) => (
-                  <li key={index} className="sm:text-sm md:text-base">
-                    {step}
+    <>
+      <div className="p-4 mt-[100px] sm:px-4 md:px-6 lg:px-8 overflow-x-hidden">
+        {data.map((algorithm) => (
+          <div key={algorithm.id} className="mb-8 sm:mb-6">
+            <h1 className="text-3xl text-purple-700 font-bold mb-4 sm:text-2xl md:text-3xl flex flex-row items-center">
+              <GiCursedStar className="text-amber-500" />
+              {algorithm.title}
+            </h1>
+            <p className="mb-4 text-lg sm:text-base">
+              <strong>Algorithm:</strong>
+              <ul className="list-disc pl-6">
+                {Array.isArray(algorithm.algorithm) ? (
+                  algorithm.algorithm.map((step: string, index: number) => (
+                    <li key={index} className="sm:text-sm md:text-base">
+                      {step}
+                    </li>
+                  ))
+                ) : (
+                  <li className="sm:text-sm md:text-base">
+                    {algorithm.algorithm}
                   </li>
-                ))
-              ) : (
-                <li className="sm:text-sm md:text-base">
-                  {algorithm.algorithm}
-                </li>
-              )}
-            </ul>
-          </p>
+                )}
+              </ul>
+            </p>
 
-          <div className="bg-gray-500 p-4 border w-full h-auto rounded-xl mockup-code sm:p-3 md:p-4 lg:p-5 overflow-x-auto">
-            <pre>
-              <code className="sm:text-sm md:text-base lg:text-lg break-words">
-                {algorithm.pseudocode}
-              </code>
-            </pre>
+            <div className="bg-gray-500 p-4 border w-full h-auto rounded-xl mockup-code sm:p-3 md:p-4 lg:p-5 overflow-x-auto">
+              <pre>
+                <code className="sm:text-sm md:text-base lg:text-lg break-words">
+                  {algorithm.pseudocode}
+                </code>
+              </pre>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+      <div className="text-2xl font-bold flex justify-center items-center text-amber-500">
+        <h1>More Algo is Cooking</h1>
+      </div>
+    </>
   );
 };
 
